245a5c84a10ee341d295f861852f024519bc9060
sourav <souravkrishna358@gmail.com>
2025-07-25 20:52:09 +0530

    made some changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import { ArrowUp } from "lucide-react";
 
 export const Footer = () => {
   return (
-    <footer className="py-12 px-4 bg-card relative border-t gap-7 border-border mt-12 pt-8 flex lg:flex-row flex-col flex-wrap lg:justify-between items-center ">
+    <footer className="py-12 px-4 bg-card relative  gap-7 border-border mt-12 pt-8 flex lg:flex-row flex-col flex-wrap lg:justify-between items-center ">
       
-      <p className="text-sm text-muted-foreground text-start lg:text-center ">
+      <p className="text-sm text-muted-foreground text-start lg:text-center lg:ml-[35%]">
         
         &copy; {new Date().getFullYear()} Souravkrishna. All rights reserved.
       </p>
